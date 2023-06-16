a18f9e667239febfa1b02f88ebc4d672331bbff0
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-17 01:38:01 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -14,7 +14,7 @@ from helper.ffmpeg import take_screen_shot,fix_thumb
 from helper.progress import humanbytes
 from helper.set import escape_invalid_curly_brackets
 
-log_channel = int(os.environ.get("LOG_CHANNEL", "-1001424450330))
+log_channel = int(os.environ.get("LOG_CHANNEL", "-1001424450330"))
 
 API_ID = int(os.environ.get("API_ID", "14091414"))
 
