335a7f225c9963c52835b96dd53c00c733f12c7e
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-17 01:35:06 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','5752952621:AAGO61IiffzN23YuXyv71fbDztA_ubGM6qo')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
